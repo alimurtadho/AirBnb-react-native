acaba8e17dcce9d3690e21c3255ea8e10ecb7388
ali murtadho <alimurtadho230196@gmail.com>
2018-08-15 14:52:39 +0700

    15/08/2018

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Airbnb Clone App
+ * @author: Andy
+ * @Url: https://www.cubui.com
+ */
+
+import React, { Component } from 'react';
+import { StatusBar, AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { Root, configureStore} from './src/navigators/AppNavigator';
+
+StatusBar.setBarStyle('light-content', true);
+
+class App extends Component {
+  render() {
+  	return (
+      <Provider store={configureStore({})}>
+        <Root />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent('App', () => App);
+
+export default App;
